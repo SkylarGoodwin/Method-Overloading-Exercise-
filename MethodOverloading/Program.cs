8ae6f1a7cdc0a0375defb4fb418c1dd178007329
SkylarGoodwin <JamesSkylarGoodwin@gmail.com>
2024-02-25 19:25:22 -0600

    added code in program.cs

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine ("enter ammount one");
+            var input = int.Parse(Console.ReadLine ()); 
+               Console.WriteLine ("enter ammount Two");
+            var inputTwo = int.Parse(Console.ReadLine ()); 
+               
+            var sum = Methods.Add(input,inputTwo);
+            Console.WriteLine(sum);
+
+            var a = 42.0m;
+			var b = 69.0m;
+			
+			var decimalAnswer = Methods.Add(a,b);
+			
+			var boolAnswer = Methods.Add(0, 0, true);
+			
+			Console.WriteLine($"decimal add: {decimalAnswer}");
+			Console.WriteLine(boolAnswer);
         }
     }
 }
